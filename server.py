c6687081e0093bbb7d4ac87ce6275231981fc620
Lev <levbox15@gmail.com>
2025-10-15 17:07:50 +0500

    Update server.py

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def chat():
                 "Content-Type": "application/json"
             },
             json=data,
-            timeout=60
+            timeout=30
         )
         return (resp.text, resp.status_code, {"Content-Type": "application/json"})
     except Exception as e:
